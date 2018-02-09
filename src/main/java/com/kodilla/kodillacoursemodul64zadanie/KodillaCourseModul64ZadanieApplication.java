a9f2fbee086b3abd3d9ec4246d8246f2c63e9951
Kaja89 <kajaspiro@gmail.com>
2018-02-09 11:51:48 +0100

    tests almost finished

---
+++ src/main/java/com/kodilla/kodillacoursemodul64zadanie/KodillaCourseModul64ZadanieApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacoursemodul64zadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseModul64ZadanieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaCourseModul64ZadanieApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kodilla/kodillacoursemodul64zadanie/KodillaCourseModul64ZadanieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.kodillacoursemodul64zadanie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaCourseModul64ZadanieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
